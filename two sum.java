fd67ea5fe312effac903f64ea2d71e839e127045
J Lokeshprabu . <132331354+lokeshtheprogrammer@users.noreply.github.com>
2025-02-07 00:11:25 +0530

    leetcode

---
+++ two sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode t= new ListNode(0);
+        ListNode d= t;
+        int ca=0;
+        while (l1!=null||l2!=null||ca!=0){
+            int s=ca;
+            if(l1!=null){
+                s=s+l1.val;
+…       return  t.next;
+    }
+   
+}
